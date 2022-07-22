b0b0256cf080712a28815e5b4f1be2342ea1f854
Somanath Jagadale <97079268+SomJagdale@users.noreply.github.com>
2022-07-22 12:23:25 +0530

    Create sprintfCrash.cpp

---
+++ POCs/sprintfCrash.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+void fun(char *nwType)
+{return ;}
+int main()
+{
+    char nwType;
+    int i;
+
+    cout<<"DefaultChar = "<<nwType<<endl;
+    cout<<"DefaultInt = "<<i<<endl;
+
+    printf("name %s, %d, %d ", "somanath", 10, "jagadale")   ;
+
+
+    return 0;
+}
