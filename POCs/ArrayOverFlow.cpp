edd09439f7f650333ecb86e31114c10b590b71e5
Somanath Jagadale <97079268+SomJagdale@users.noreply.github.com>
2022-07-22 12:16:22 +0530

    Create ArrayOverFlow.cpp

---
+++ POCs/ArrayOverFlow.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int lock=0;
+    char buf[2] = {0};
+
+    buf[0]='s';
+    buf[1]='j';
+    buf[2]='j';
+
+    cout<<" lock = "<<lock<<" buf ="<<buf<<endl;
+
+    return 0;
+}
