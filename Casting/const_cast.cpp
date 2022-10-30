c94648a02a4070f8f8e391a3b44b158cb28a780d
Somanath Jagadale <97079268+SomJagdale@users.noreply.github.com>
2022-10-31 00:06:07 +0530

    Update const_cast.cpp

--- Casting/const_cast.cpp
+++ Casting/const_cast.cpp
@@ -1,4 +1,7 @@
 
+static_const -  it removes the constness of the pointer variable 
+ const_cast
+const_cast is used to cast away the constness of variables. Following are some interesting facts about const_cast.
 #include <iostream>
 
 using namespace std;
