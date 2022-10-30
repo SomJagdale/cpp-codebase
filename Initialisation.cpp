6ef5094d40798943bbd5a0d72cb58df9bdb364e1
Somanath Jagadale <97079268+SomJagdale@users.noreply.github.com>
2022-10-30 22:33:57 +0530

    Create Initialisation.cpp

---
+++ Initialisation.cpp
@@ -0,0 +1,15 @@
+// Different ways to intialise variable
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a(5);
+    int b{5};
+
+    cout<<"Hello World - "<<a<<" b -"<<b;
+    return 0;
+}
+
